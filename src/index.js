5daae413af54aef5eece73c3bda41ae2115845f3
cjq <2598158582@qq.com>
2019-04-18 09:03:14 +0800

    22

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// 1.Require能方法接受一个数组和一个回调函数将模块引入
+// 2.Define定义的模块能够正确返回内容
+// 3.可以利用该逻辑引入js文件
+// 4.正确配置git，并能提交至远程服务器
+
+function require(Array) {
+    //导入模块
+    if (typeof object === Array) {
+        return Array
+    }
+}
+require([1, 2, 3], {
+    'baseUrl': "paths"
+})
+
+
+function defined(rs) {
+    //定义模块
+    let req = object;
+    if (typeof object === object) {
+        return rs
+    }
+}
